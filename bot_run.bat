ef43864a7de59eb095e0e3b1a522dbe30180995f
Petr <baryshev.petr@yandex.ru>
2022-11-30 12:16:47 +0300

    Added bot_run.bat

---
+++ bot_run.bat
@@ -0,0 +1,12 @@
+@echo off
+
+call C:\Users\PC\PycharmProjects\tgbot\venv\Scripts\activate
+
+cd C:\Users\PC\PycharmProjects\tgbot
+
+set TOKEN_BOT= #TOKEN_BOT
+set TOKEN_YT= #TOKEN_YT
+
+python main.py
+
+pause
